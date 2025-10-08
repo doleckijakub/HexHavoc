99c88a93e8780d879ca1d49351e2c42d9aac2219
Jakub Dolecki <49841337+doleckijakub@users.noreply.github.com>
2025-10-08 22:00:21 +0200

    Create index.ts

---
+++ client/src/index.ts
@@ -0,0 +1,157 @@
+interface Vec2 {
+    x: number,
+    y: number,
+}
+
+enum TerrainTileType {
+    EMPTY,
+    GRASS,
+    DIRT,
+    SAND,
+    CLAY,
+}
+
+enum TerrainEntityType {
+    TREE,
+    STONE,
+}
+
+type EntityType = TerrainEntityType /* | ... */;
+
+class Entity {
+    id: string;
+    position: Vec2;
+    ty: EntityType;
+}
+
+interface EntityMovePacket {
+    packet_type: 'entity_move'
+    id: number,
+    new_position: Vec2,
+}
+
+interface TerrainChunk {
+    position: Vec2,
+    contents: TerrainTileType[],
+}
+
+interface TerrainChunkPacket { // sent whenever the player "loads it"
+    packet_type: 'terrain_chunk',
+    chunk: TerrainChunk,
+}
+
+interface EntityLoadPacket {
+    packet_type: 'entity_load',
+    entity: Entity,
+}
+
+interface EntityUnloadPacket {
+    packet_type: 'entity_unload',
+    id: string,
+}
+
+interface PlayerRegisterPacket {
+    packet_type: 'player_register';
+    username: string;
+}
+
+interface PlayerRegisteredPacket {
+    packet_type: 'player_registered',
+    id: string,
+}
+
+type Packet = 
+    EntityMovePacket | EntityLoadPacket | EntityUnloadPacket |
+    TerrainChunkPacket |
+    PlayerRegisterPacket | PlayerRegisteredPacket;
+
+
+
+const CONFIG = {
+    KEY_UP: 'w',
+    KEY_DOWN: 's',
+    KEY_LEFT: 'a',
+    KEY_RIGHT: 'd'
+};
+
+/* Do rozważenia może później:
+enum ClientState {
+    INITIAL,
+    AWAITING_START,
+    IN_GAME,
+};
+*/
+
+const WS_URL = 'ws://localhost:3000';
+
+class Game {
+    private ws: WebSocket;
+
+    private canvas: HTMLCanvasElement;
+    private cc: CanvasRenderingContext2D;
+    private player_id?: string;
+
+    constructor({ wsUrl }: { wsUrl: string }) {
+        this.ws = new WebSocket(wsUrl);
+
+        this.ws.onmessage = (ev: MessageEvent<any>): void => {
+            this.recv(JSON.parse(ev.data))
+        }
+    }
+
+    private recv(packet: Packet): void {
+        switch (packet.packet_type) {
+            case 'player_registered': this.onPlayerRegistered(packet); break;
+        }
+    }
+
+    private onPlayerRegistered(packet: PlayerRegisteredPacket): void {
+        this.player_id = packet.id;
+    }
+
+    private send(packet: Packet): void {
+        this.ws.send(JSON.stringify(packet));
+    }
+
+    private updatePlaying() {
+        // rendering
+
+        // update logic: interpolate movement
+        /// my player movement
+    }
+
+    private update(timestamp: DOMHighResTimeStamp) {
+        if (this.player_id) {
+            this.updatePlaying();
+        } else {
+            /// render text that you are waiting for ACK
+        }
+
+        requestAnimationFrame(this.update);
+    }
+
+    public run() {
+        const registerPacket: PlayerRegisterPacket = {
+            packet_type: 'player_register',
+            username: 'mdd',
+        };
+
+        this.send(registerPacket);
+
+        this.canvas = document.querySelector('canvas');
+        this.cc = this.canvas.getContext('2d');
+
+        const keyboard = {};
+
+        function isKeyPressed(key: number): boolean {
+            return keyboard[key] || false;
+        }
+
+        document.addEventListener('keydown', event => keyboard[event.key] = true);
+        document.addEventListener('keyup', event => keyboard[event.key] = false);
+
+        const terrain = {}; // "0,0": {}, "1,0": {}
+
+        requestAnimationFrame(this.update);
+    }
+}
