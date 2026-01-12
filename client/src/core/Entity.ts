d0b02f74b5e4150fe62411a7828186e60f4e74ee
Michał Dziuba <mail@michaldziuba.dev>
2026-01-12 23:52:19 +0100

    feat: integrate first assets batch into code

--- client/src/core/Entity.ts
+++ client/src/core/Entity.ts
@@ -19,6 +19,62 @@ export class EntityForestTree extends Entity {
     entity_type: 'forest_tree';
 }
 
+export class EntitySpruceTree extends Entity {
+    entity_type: 'spruce_tree';
+}
+
+export class EntityJungleTree extends Entity {
+    entity_type: 'jungle_tree';
+}
+
+export class EntityCactus extends Entity {
+    entity_type: 'cactus';
+}
+
+export class EntityTreeStump extends Entity {
+    entity_type: 'tree_stump';
+}
+
+export class EntityIceSpike extends Entity {
+    entity_type: 'ice_spike';
+}
+
+export class EntityBush extends Entity {
+    entity_type: 'bush';
+}
+
+export class EntityStone extends Entity {
+    entity_type: 'stone';
+}
+
+export class EntityBigStone extends Entity {
+    entity_type: 'big_stone';
+}
+
+export class EntityTreeLog extends Entity {
+    entity_type: 'tree_log';
+}
+
+export class EntityTallGrass extends Entity {
+    entity_type: 'tall_grass';
+}
+
+export class EntitySeaShell extends Entity {
+    entity_type: 'sea_shell';
+}
+
+
 export type EntityType =
     EntityPlayer |
-    EntityForestTree;
+    EntityForestTree |
+    EntitySpruceTree |
+    EntityJungleTree |
+    EntityCactus |
+    EntityTreeStump |
+    EntityIceSpike |
+    EntityBush |
+    EntityStone |
+    EntityBigStone |
+    EntityTreeLog |
+    EntityTallGrass |
+    EntitySeaShell;
